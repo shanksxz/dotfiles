10fb8549a140598bb04e3834c43697ba98d046c8
somya bhatt <somyacu1@gmail.com>
2024-08-27 06:39:58 +0530

    fix: format and re-use mappings

--- nvim/lua/custom/init.lua
+++ nvim/lua/custom/init.lua
@@ -1,91 +1,90 @@
-vim.opt.relativenumber = true
+local options = {
+	relativenumber = true,
+	tabstop = 4,
+	softtabstop = 4,
+	shiftwidth = 4,
+	expandtab = true,
+	termguicolors = true,
+	clipboard = "unnamedplus",
+}
+
 vim.g.mapleader = " "
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.termguicolors = true
-vim.opt.clipboard = "unnamedplus"
 
--- better focus for vim panes
-vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", { noremap = true })
-vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", { noremap = true })
-vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", { noremap = true })
-vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", { noremap = true })
-vim.keymap.set("t", "jk", [[<C-\><C-n>]])
-vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]])
-vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]])
-vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]])
-vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]])
-vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]])
+local function map(mode, lhs, rhs, opts)
+	local options = { noremap = true, silent = true }
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
+	vim.keymap.set(mode, lhs, rhs, options)
+end
 
--- move entire line up/down in both normal and visual mode
-vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { noremap = true })
-vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { noremap = true })
-vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { noremap = true })
-vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true })
+-- window navigation
+map("n", "<C-k>", ":wincmd k<CR>", { desc = "Move to upper window" })
+map("n", "<C-j>", ":wincmd j<CR>", { desc = "Move to lower window" })
+map("n", "<C-h>", ":wincmd h<CR>", { desc = "Move to left window" })
+map("n", "<C-l>", ":wincmd l<CR>", { desc = "Move to right window" })
+map("t", "jk", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
+map("t", "<C-h>", [[<Cmd>wincmd h<CR>]], { desc = "Move to left window from terminal" })
+map("t", "<C-j>", [[<Cmd>wincmd j<CR>]], { desc = "Move to lower window from terminal" })
+map("t", "<C-k>", [[<Cmd>wincmd k<CR>]], { desc = "Move to upper window from terminal" })
+map("t", "<C-l>", [[<Cmd>wincmd l<CR>]], { desc = "Move to right window from terminal" })
+map("t", "<C-w>", [[<C-\><C-n><C-w>]], { desc = "Window command in terminal mode" })
+
+-- move lines
+map("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+map("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
 
 -- splits
-vim.api.nvim_set_keymap("n", "<leader>sh", ":split<CR>", { noremap = true, silent = true, desc = "Split horizontally" })
-vim.api.nvim_set_keymap("n", "<leader>sv", ":vsplit<CR>", { noremap = true, silent = true, desc = "Split vertically" })
+map("n", "<leader>sh", ":split<CR>", { desc = "Split horizontally" })
+map("n", "<leader>sv", ":vsplit<CR>", { desc = "Split vertically" })
+map("n", "<leader>sc", ":close<CR>", { desc = "Close split" })
+map("n", "<leader>so", ":only<CR>", { desc = "Close all splits except current" })
+map("n", "<C-Left>", ":vertical resize -2<CR>", { desc = "Resize vertically left" })
+map("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "Resize vertically right" })
+map("n", "<C-Up>", ":resize -2<CR>", { desc = "Resize horizontally up" })
+map("n", "<C-Down>", ":resize +2<CR>", { desc = "Resize horizontally down" })
 
--- resize splits
-vim.api.nvim_set_keymap(
-	"n",
-	"<C-Left>",
-	":vertical resize -2<CR>",
-	{ noremap = true, silent = true, desc = "Resize vertically left" }
-)
-vim.api.nvim_set_keymap(
-	"n",
-	"<C-Right>",
-	":vertical resize +2<CR>",
-	{ noremap = true, silent = true, desc = "Resize vertically right" }
-)
-vim.api.nvim_set_keymap(
-	"n",
-	"<C-Up>",
-	":resize -2<CR>",
-	{ noremap = true, silent = true, desc = "Resize horizontally up" }
-)
-vim.api.nvim_set_keymap(
-	"n",
-	"<C-Down>",
-	":resize +2<CR>",
-	{ noremap = true, silent = true, desc = "Resize horizontally down" }
-)
+-- terminal
+map("n", "<leader>th", "<cmd>ToggleTerm direction=horizontal<CR>", { desc = "Toggle terminal horizontally" })
+map("n", "<leader>tv", "<cmd>ToggleTerm direction=vertical<CR>", { desc = "Toggle terminal vertically" })
+map("n", "<leader>tf", "<cmd>ToggleTerm direction=float<CR>", { desc = "Toggle terminal float" })
+map("t", "<C-d>", "<C-\\><C-n>:bd!<CR>", { desc = "Exit and close terminal" })
 
-vim.api.nvim_set_keymap("n", "<leader>sc", ":close<CR>", { noremap = true, silent = true, desc = "Close split" })
-vim.api.nvim_set_keymap(
-	"n",
-	"<leader>so",
-	":only<CR>",
-	{ noremap = true, silent = true, desc = "Close all splits except current" }
-)
+-- buffers
+map("n", "<leader>bn", "<:BufferLineCycleNext<CR>", { noremap = true, silent = true, desc = "Next buffer" })
+map("n", "<leader>bp", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true, desc = "Previous buffer" })
+map("n", "<leader>bd", ":bdelete<CR>", { noremap = true, silent = true, desc = "Delete buffer" })
 
--- terminal toggle
-vim.keymap.set(
-	"n",
-	"<leader>th",
-	"<cmd>ToggleTerm direction=horizontal<CR>",
-	{ noremap = true, silent = true, desc = "Toggle terminal horizontally" }
-)
-vim.keymap.set(
-	"n",
-	"<leader>tv",
-	"<cmd>ToggleTerm direction=vertical<CR>",
-	{ noremap = true, silent = true, desc = "Toggle terminal vertically" }
-)
-vim.keymap.set("t", "<C-d>", "<C-\\><C-n>:bd!<CR>", { noremap = true, silent = true, desc = "Exit and close terminal" })
-vim.keymap.set(
-	"n",
-	"<leader>tf",
-	"<cmd>ToggleTerm direction=float<CR>",
-	{ noremap = true, silent = true, desc = "Toggle terminal float" }
-)
+-- yank
+map("n", "<leader>y", '"+y', { noremap = true, desc = "Yank to system clipboard" })
+map("v", "<leader>y", '"+y', { noremap = true, desc = "Yank selection to system clipboard" })
 
--- was messing around with this, but it's not really useful ig
-function switch_case(v)
+-- noice
+map("n", "<leader>nd", "<cmd>NoiceDismiss<CR>", { desc = "Dismiss Noice Message" })
+
+-- source curr file
+map("n", "<leader><leader>", "<cmd>luafile %<CR>", { desc = "Source current file" })
+
+-- exit insert mode without hitting Esc
+map("i", "jj", "<Esc>", { desc = "Esc" })
+
+-- select all
+map("n", "==", "ggVG", { desc = "Select all" })
+
+-- theme-switch
+map("n", "<leader>tt", ":Themery<CR>", { noremap = true, silent = true, desc = "Change Themes" })
+
+-- zen-mode
+map("n", "<leader>z", ":ZenMode<CR>", { noremap = true, silent = true, desc = "ZenMode" })
+
+for k, v in pairs(options) do
+	vim.opt[k] = v
+end
+
+-- run file
+local function switch_case(v)
 	local cases = {
 		["javascript"] = "node",
 		["typescript"] = "ts-node",
@@ -100,41 +99,5 @@ vim.keymap.set("n", "<leader>r", function()
 	local cmd = switch_case(filetype)
 	local filename = vim.fn.expand("%")
 	local full_cmd = cmd .. " " .. filename
-
 	vim.cmd('TermExec cmd="' .. full_cmd .. '" direction=float go_back=0')
 end, { noremap = true, silent = true, desc = "Run file" })
-
--- vim.keymap.set("n", "<leader>r", function()
--- 	local filetype = vim.bo.filetype
--- 	local cmd = switch_case(filetype)
--- 	local filename = vim.fn.expand("%")
--- 	local full_cmd = cmd .. " " .. filename
---
--- 	local pickers = require("telescope.pickers")
--- 	local finders = require("telescope.finders")
--- 	local conf = require("telescope.config").values
---
--- 	local output = vim.fn.systemlist(full_cmd)
---
--- 	pickers
--- 		.new({}, {
--- 			prompt_title = "Command Output",
--- 			finder = finders.new_table({
--- 				results = output,
--- 				entry_maker = function(entry)
--- 					return {
--- 						value = entry,
--- 						display = entry,
--- 						ordinal = entry,
--- 					}
--- 				end,
--- 			}),
--- 			sorter = conf.generic_sorter({}),
--- 			previewer = false, -- Disable the previewer
--- 			layout_config = {
--- 				width = 0.8,
--- 				height = 0.8,
--- 			},
--- 		})
--- 		:find()
--- end, { noremap = true, silent = true, desc = "Run file" })
